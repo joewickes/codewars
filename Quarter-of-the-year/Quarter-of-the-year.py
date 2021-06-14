12c8844e46d8aa550034ed500e09536dc79f2abf
joewickes <joe.c.wickes@gmail.com>
2021-06-14 08:40:25 -0400

    Solve Quarter of the year in Python

--- Quarter-of-the-year/Quarter-of-the-year.py
+++ Quarter-of-the-year/Quarter-of-the-year.py
@@ -1,2 +1,7 @@
 def quarter_of(month):
-    # your code here
+  if type(month) == int :
+    return (1 if month == 1 or month == 2 or month == 3 
+        else 2 if month == 4 or month == 5 or month == 6
+        else 3 if month == 7 or month == 8 or month == 9
+        else 4 if month == 10 or month == 11 or month == 12
+        else 'Not a valid month number')

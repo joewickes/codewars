f914437bdadf398f9594d96c89db27414cb69baa
joewickes <joe.c.wickes@gmail.com>
2021-06-14 08:22:14 -0400

    Solve Quarter of the year in JS

--- Quarter-of-the-year/Quarter-of-the-year.js
+++ Quarter-of-the-year/Quarter-of-the-year.js
@@ -1,4 +1,9 @@
-Quartconst quarterOf = (month) => {
-  // Your code here
-  
+const quarterOf = (month) => {
+  if (typeof month === 'number') {
+    return month === 1 || month === 2 || month === 3 ? 1
+      : month === 4 || month === 5 || month === 6 ? 2
+      : month === 7 || month === 8 || month === 9 ? 3
+      : month === 10 || month === 11 || month === 12 ? 4
+      : 'Not a valid month number'
+  }
 }

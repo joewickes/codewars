89e59cbf6e10799e63046acca293a72a10b09e99
joewickes <joe.c.wickes@gmail.com>
2021-10-18 09:42:58 -0400

    Solve 2 kata in TS

---
+++ Opposites-attract/Opposites-attract.ts
@@ -0,0 +1,1 @@
+export const lovefunc = (flower1: number, flower2: number): boolean => !(flower1 % 2 === flower2 % 2);
---
+++ Watermelon/Watermelon.ts
@@ -0,0 +1,1 @@
+export const divide = (weight: number): boolean => !(weight % 2);
